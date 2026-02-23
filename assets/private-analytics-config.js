5bb5e99bf0a37b7522d890289fdf85aed6e1c9c6
Thistle W <han@Thistles-MacBook-Pro-2.local>
2026-02-23 14:18:58 +0800

    Enable Statcounter tracking with project config

--- assets/private-analytics-config.js
+++ assets/private-analytics-config.js
@@ -1,10 +1,10 @@
 window.__PRIVATE_ANALYTICS__ = {
-  enabled: false,
+  enabled: true,
   onlyOnDomains: ["cehw.github.io"],
   oncePerSession: false,
   statcounter: {
-    project: 0,
-    security: "",
+    project: 13206449,
+    security: "2b51caf0",
     invisible: 1,
     removeLink: true,
   },
